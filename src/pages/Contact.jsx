0a04535a36c0da8c848a6e1066969345bac05f8c
Oli Nelson <olivernelson@icloud.com>
2020-01-07 21:02:37 -0500

    added ajax form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Form, Button, Header } from "semantic-ui-react";
 
 import styled from "styled-components";
+import ContactForm from "../components/ContactForm";
 
 export default function Contact() {
   const CenteredContainer = styled(Container)`
@@ -13,36 +14,7 @@ export default function Contact() {
 
   return (
     <CenteredContainer text>
-      <Form
-        inverted
-        action='https://formspree.io/olinelson93@gmail.com'
-        method='POST'
-      >
-        <Header as='h1' inverted>
-          Contact
-        </Header>
-        <Form.Input
-          required
-          label='Full Name'
-          name='name'
-          placeholder='Mike Nock'
-        />
-        <Form.Input
-          required
-          label='Email'
-          name='email'
-          type='email'
-          placeholder='pocket_science@gmail.com'
-        />
-
-        <Form.TextArea
-          required
-          name='message'
-          label='Message'
-          placeholder='It Was the Best of Times; It Was the Worst of Times...'
-        />
-        <Button type='submit'>Send</Button>
-      </Form>
+      <ContactForm />
     </CenteredContainer>
   );
 }
